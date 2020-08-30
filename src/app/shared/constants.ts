cd25356226fe8a1cb63d1aaa37fe66ea98b011aa
Jonathan Mayol <mayoljonathan@gmail.com>
2020-08-30 20:00:34 +0800

    update IS_AVAILABLE

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-  static IS_AVAILABLE = true;
+  static IS_AVAILABLE = false;
   static EMAIL_ADDRESS = 'mayoljonathan@gmail.com';
   static RESUME_URL = 'https://drive.google.com/file/d/17-VSA2DlBkJgQcPMQqAIKsjMrPHi7IE_/view';
 }
